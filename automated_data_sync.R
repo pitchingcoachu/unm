8bab28b395575e8993a5114e6fb738a93e60b82f
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-04-09 09:31:15 -0700

    Update automated_data_sync.R

--- automated_data_sync.R
+++ automated_data_sync.R
@@ -167,7 +167,7 @@ is_date_in_range <- function(file_path) {
   file_date <- as.Date(paste(date_match[2], date_match[3], date_match[4], sep = "-"))
   
   # Start date: August 1, 2025 (nothing before this)
-  start_date <- as.Date("2025-08-10")
+  start_date <- as.Date("2026-03-24")
   
   # Include all data from August 1, 2025 onwards (no future year restrictions)
   return(file_date >= start_date)
